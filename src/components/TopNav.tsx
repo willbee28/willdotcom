6438b7692a08892e046cfcaa8f3a589c7e935e46
Will <will.t.borwegen@gmail.com>
2024-12-26 23:42:48 -0600

    fix the nav submenu in mobile mode (again)

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -5,7 +5,7 @@ function TopNav() {
   const [isNavOpen, setNavOpen] = useState(false);
   return (
     <div className="text-[#FEFAE0]">
-      <div className="sticky top-0 py-2 px-8 text-xl bg-[#283618] z-10">
+      <div className="fixed w-full py-2 px-8 text-xl bg-[#283618] z-10">
         <div className="flex sm:w-11/12 mx-auto items-center justify-between">
           <div className="cols-3 flex gap-4 font-semibold text-3xl">
             <img width="40px" height="40px" src="/bear.svg"></img>
@@ -28,18 +28,20 @@ function TopNav() {
       </div>
       {/* Mobile menu */}
       <div
-        className={`bg-[#283618] fixed right-0 w-44 text-3xl p-4
+        className={`sm:hidden bg-[#283618] fixed right-0 w-44 text-3xl
           shadow-lg transform transition-transform duration-300 ${
-            isNavOpen ? "translate-y-0" : "-translate-y-full"
+            isNavOpen ? "translate-y-16" : "-translate-y-full"
           }`}
       >
-        <a
-          href="/resume-borwegen.pdf"
-          className="hover:underline"
-          target="_blank"
-        >
-          Resume
-        </a>
+        <div className="flex flex-col px-4 py-2">
+          <a
+            href="/resume-borwegen.pdf"
+            className="hover:underline"
+            target="_blank"
+          >
+            Resume
+          </a>
+        </div>
       </div>
     </div>
   );
