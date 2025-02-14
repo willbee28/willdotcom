c04126c786e2a82eb1b2a26fdcfcee270bf5f563
Will <will.t.borwegen@gmail.com>
2025-02-14 12:01:47 -0500

    sort images chronologically and make image viewing more mobile friendly

--- src/components/PctTravels.tsx
+++ src/components/PctTravels.tsx
@@ -2,24 +2,24 @@ const PctTravels = () => {
   const prelimImageUrls: string[] = [];
   const imageUrls: string[] = [];
   for (let i = 1; i < 9; i++) {
-    const imagePath = `/smallPhotos/${i}.jpeg`;
+    const imagePath = `/pctPhotos/${i}.jpeg`;
     prelimImageUrls.push(imagePath);
   }
   for (let i = 9; i < 151; i++) {
-    const imagePath = `/smallPhotos/${i}.jpeg`;
+    const imagePath = `/pctPhotos/${i}.jpeg`;
     imageUrls.push(imagePath);
   }
 
   return (
     <div>
-      <div className="w-3/5 mx-auto my-24 sm:my-64 text-[#283618] text-3xl leading-relaxed">
+      <div className="w-3/5 mx-auto mt-36 sm:my-64 text-[#283618] text-3xl leading-relaxed">
         <div>
           <span className="font-bold">Below</span> you can find picture of my
           travels from Mexico to Canada along the Pacific Crest Trail
         </div>
       </div>
       <div className="mt-24 mx-auto w-4/5  ">
-        <div className="grid grid-cols-2 gap-2 p-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 p-2">
           {prelimImageUrls.map((imageUrl, index) => (
             <img
               key={index}
@@ -31,9 +31,9 @@ const PctTravels = () => {
         </div>
         <div className="w-3/5 mx-auto my-8 sm:my-24 text-[#283618] text-3xl leading-relaxed">
           <div className="mb-12">This journey was really something else</div>
-          <div className="flex justify-end mr-36">Hope you enjoy</div>
+          <div className="flex justify-end">Hope you enjoy</div>
         </div>
-        <div className="grid grid-cols-2 gap-2 p-2 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 p-2 ">
           {imageUrls.map((imageUrl, index) => (
             <img
               key={index}
