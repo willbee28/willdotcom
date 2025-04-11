6a1028e10cc73c1526357a4eb9a6a0420cc2ae3b
Will <will.t.borwegen@gmail.com>
2025-04-11 13:09:58 -0400

    add shadow to website cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,15 +3,17 @@ import { motion } from "framer-motion";
 function Projects() {
   const projects = [
     {
-      title: "Cool App",
+      title: "My Pacific Crest Trail Experience",
       image: "/fullSiteScreenshots/pct_site_cropped.png",
-      description: "A really cool app I built to solve X problem.",
+      description:
+        "A small app I built to display my favorite photos from a 6-month solo journey along the Pacific Crest Trail.",
       link: "https://wborwegen.com/pct",
     },
     {
-      title: "Design Tool",
+      title: "150th Anniversary",
       image: "/fullSiteScreenshots/lighthouse_site.png",
-      description: "A design tool made with love and React.",
+      description:
+        "A website I made for a client who needed a site to celebrate the 150th anniversary of a lighthouse, with event information and tickets.",
       link: "https://thomaspoint150th.com/",
     },
     // Add more projects as needed
@@ -33,21 +35,30 @@ function Projects() {
 
         <div className="grid gap-8 grid-cols-2">
           {projects.map((project, index) => (
-            <div key={index} className="bg-[#343634] rounded-md p-5">
-              <div className="h-72 overflow-hidden rounded-sm">
-                <a
-                  href={project.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+            <a
+              href={project.link}
+              className=""
+              key={index}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <div
+                key={index}
+                className="bg-[#343634] rounded-md p-5 hover:scale-101 duration-300 hover:shadow-[0_10px_25px_rgba(0,0,0,0.4)] transition-all ease-in "
+              >
+                <div className="text-[#fefae0] h-24">
+                  {/* <div className="text-xl ">{project.title}</div> */}
+                  <div className="">{project.description}</div>
+                </div>
+                <div className="h-72 overflow-hidden rounded-sm border-[#fefae0] border-1">
                   <img
                     src={project.image}
                     alt={project.title}
                     className="w-full object-cover image-animated"
                   />
-                </a>
+                </div>
               </div>
-            </div>
+            </a>
           ))}
         </div>
       </div>
